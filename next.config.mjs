657a892cb3b7a4f29c84dedf66d93fbe997e5310
yuto ichinose <yuto.0526.sano@gmail.com>
2024-04-16 12:55:31 +0900

    feat: mdx表示完了

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs (notice the filename change)
 
-export default nextConfig;
+import withMDX from "@next/mdx";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Configure pageExtensions to include md and mdx
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    // Optionally, add any other Next.js config below
+    reactStrictMode: true,
+};
+
+// Merge MDX config with Next.js config
+export default withMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, this configuration remains the same.
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})(nextConfig);
